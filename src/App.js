21eea677d8cc59f428dccc2fccf417fa0a1469c7
federica <fdelgadodeleon@linti.unlp.edu.ar>
2020-11-09 11:12:01 -0300

    Implemented login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="container">
+        <div className="image-container">
+          <img src="/img/foto.jpg" className="image"/>
+        </div>
+        <div className="form">
+          <input placeholder="usuario"/>
+          <input placeholder="contraseña"/>
+          <button>Iniciar sesión</button>
+        </div>
+        </div>
     </div>
   );
 }
